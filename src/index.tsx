b6fa6b8be08be2cfb4514632e809be71f23ac1e2
Aditya Sabnis <adityasabnis@gmail.com>
2021-08-01 20:56:03 +1000

    fix: 100% coverage + cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./components/App";
 
-ReactDOM.render(
+export const ReactStrictMode = (
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
+
+export const rootElement = document.getElementById("root");
+
+ReactDOM.render(ReactStrictMode, rootElement);
